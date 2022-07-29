bd2e0cb9525db7440a78f4f1159f82d76de5aaf2
Kasperi Kaivola <antti.k.kaivola@aalto.fi>
2022-07-29 03:34:29 +0300

    Ex. 2.12 done

---
+++ part2/countrydata/src/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import FilteredPersons from './components/FilteredPersons'
+import Search from './components/Search'
+import AddNumber from './components/AddNumber'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => setCountries(response.data))
+  }, [])
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Search searchTerm={searchTerm} handleSearchChange={handleSearchChange}/>
+      <h3>Countries</h3>
+      <FilteredPersons persons={countries} filter={searchTerm}/>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/countrydata/src/components/FilteredPersons.js
@@ -0,0 +1,24 @@
+const FilteredPersons = ({persons,filter}) => {
+    const filtered = persons.filter(p => p.name.common.toLowerCase().includes(filter.toLowerCase()))
+    if(filtered.length<=10 && filtered.length>1) return filtered.map(p => <p key={p.name.common}>{p.name.common} {p.number}</p>)
+    else if(filtered.length===1) {
+      const cand = filtered[0]
+      return (
+        <div>
+          <h1>{cand.name.common}</h1>
+          <p>Capital {cand.capital}</p>
+          <p>Area {cand.area}</p>
+          <h4><b>Languages:</b></h4>
+          {console.log(Object.values(cand.languages))}
+          <ul>
+            {Object.values(cand.languages).map(lang => <li key={lang}>{lang}</li>)}
+          </ul>
+          <img src={cand.flags.png} height="200" width ="200"/>
+        </div>
+      )
+    }
+    else if(filtered.length===0) return (<p>No matches, specify another filter</p>)
+    else return (<p>Too many matches, specify another filter</p>)
+  }
+  
+  export default FilteredPersons
