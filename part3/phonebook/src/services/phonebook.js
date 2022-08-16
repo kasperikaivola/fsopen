9f18d00ec5ac860abadbe64ed3c7963b01a33270
kasperikaivola <98049597+kasperikaivola@users.noreply.github.com>
2022-08-16 18:45:07 +0300

    Ex. 3.9-3.10 frontend done

--- part3/phonebook/src/services/phonebook.js
+++ part3/phonebook/src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://radiant-garden-13683.herokuapp.com/api/persons'
 
 const getAll = () => {return axios.get(baseUrl)}
 
